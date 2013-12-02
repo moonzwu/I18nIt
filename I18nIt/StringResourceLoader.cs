8cf43417c01903413ea6a425ec1c29407e668e02
moonzwu <moonz.wu@gmail.com>
2013-12-02 13:49:00 +0800

    add check string resource file type

---
+++ I18nIt/StringResourceLoader.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+
+namespace I18nIt
+{
+    public class StringResourceLoader
+    {
+        private Dictionary<string, string> _resourceMap = new Dictionary<string, string>();
+ 
+        public static StringResouceType DecideResourceType(string file)
+        {
+            var extension = Path.GetExtension(file);
+            if (!File.Exists(file) || extension == null) return StringResouceType.Unknown;
+            if (extension.Equals(".properties", StringComparison.InvariantCultureIgnoreCase))
+            {
+                return StringResouceType.JavaStyle;
+            }
+
+            if (extension.Equals(".mpx", StringComparison.InvariantCultureIgnoreCase))
+            {
+                return StringResouceType.MPStyle;
+            }
+            return StringResouceType.Unknown;
+        }
+    }
+}
---
+++ I18nItTest/StringResourceLoaderTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using I18nIt;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace I18nItTest
+{
+    [TestClass]
+    public class StringResourceLoaderTest
+    {
+        [TestMethod]
+        public void Should_get_correct_resource_type_when_given_a_file_path()
+        {
+            const string javaResource = "com_lenovo_plugin_app.properties";
+            var mpResource = "English.LanguagePack.Fragment.mpx";
+            var otherResource = "abcd.xxx";
+
+            var javaFile = CreateATempFile(javaResource);
+            var resouceType1 = StringResourceLoader.DecideResourceType(javaFile);
+            Assert.AreEqual(StringResouceType.JavaStyle, resouceType1);
+
+            var mpFile = CreateATempFile(mpResource);
+            var resouceType2 = StringResourceLoader.DecideResourceType(mpFile);
+            Assert.AreEqual(StringResouceType.MPStyle, resouceType2);
+
+            var otherFile = CreateATempFile(otherResource);
+            var resouceType3 = StringResourceLoader.DecideResourceType(otherFile);
+            Assert.AreEqual(StringResouceType.Unknown, resouceType3);
+        }
+
+        private string CreateATempFile(string name)
+        {
+            var fileName = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName() + name);
+            using (new FileStream(fileName, FileMode.CreateNew)) { }
+            return fileName;
+        }
+    }
+}
